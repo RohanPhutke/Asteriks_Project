0e991946f92a84f4c6111f272b3e8ffea744eb6b
Anand Raj <iit2022047@iiita.ac.in>
2024-04-20 19:10:39 +0530

    Css correction

--- client/src/pages/Student_room_all.jsx
+++ client/src/pages/Student_room_all.jsx
@@ -192,27 +192,26 @@ const Studentroomall = () => {
           </form>
         </div>
         <div className={styles.occupancys}>
-          <a href="id123">
-            <span>
-              Hostel Updates
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={16}
-                height={16}
-                fill="currentColor"
-                className="bi bi-arrow-up-right-square-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707" />
-              </svg>
-            </span>
-          </a>
+          <span>
+            Hostel Updates
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={16}
+              height={16}
+              fill="currentColor"
+              className="bi bi-arrow-up-right-square-fill"
+              viewBox="0 0 16 16"
+            >
+            </svg>
+          </span>
           <div className={styles.occupancys_border}>
-            <StudentRoomComp hostelNo={1} />
-            <StudentRoomComp hostelNo={2} />
-            <StudentRoomComp hostelNo={3} />
-            <StudentRoomComp hostelNo={4} />
-            <StudentRoomComp hostelNo={5} />
+            <div className={styles.leftalien}>
+              <StudentRoomComp hostelNo={1} />
+              <StudentRoomComp hostelNo={2} />
+              <StudentRoomComp hostelNo={3} />
+              <StudentRoomComp hostelNo={4} />
+              <StudentRoomComp hostelNo={5} />
+            </div>
           </div>
         </div>
       </div>

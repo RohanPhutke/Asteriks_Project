ebfc046c0503999b6004a3ff9bf6ef33bca7d8c5
piyush <piyushpriyadarshi1020@gmail.com>
2024-04-22 23:37:09 +0530

    Notification Box Added

--- client/src/pages/Student_home.jsx
+++ client/src/pages/Student_home.jsx
@@ -15,10 +15,15 @@ const Studenthome = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [editedText, setEditedText] = useState('');
   const [value, onChange] = useState(new Date());
-  const [showCalendar, setShowCalendar] = useState(false); // State to control calendar visibility
+  const [showCalendar, setShowCalendar] = useState(false); 
+  const [shownotify, setShownotify] = useState(false); 
 
   const toggleCalendar = () => {
     setShowCalendar(!showCalendar);
+  };
+
+  const togglenotification = () => {
+    setShownotify(!shownotify);
   };
 
   const showToast = () => {
@@ -185,13 +190,22 @@ const Studenthome = () => {
             fill="currentColor"
             className="bi bi-bell-fill"
             viewBox="0 0 16 16"
-            onClick={showToast}
+            onClick={togglenotification}
             style={{ cursor: 'pointer' }}
           >
             <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
           </svg>
           <a href="/loginStudent">
-          
+          {shownotify && (
+            <div className={styles.Notification_box}>
+              <div className={styles.Notification_box_header}>Notifications</div>
+              <div className={styles.Notification_box_content}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+             <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
+             </svg>
+                Your notifications live here</div>
+            </div>
+           )}
         <svg 
           xmlns="http://www.w3.org/2000/svg" 
           width={42} 
